d6b9b9b177816f3df9f2952b61f84becec60c3ce
Derrick Zhen <dzhen001@gmail.com>
2020-09-13 02:52:57 -0400

    -

--- src/components/intro.js
+++ src/components/intro.js
@@ -48,7 +48,7 @@ export default function Intro() {
             <i class="fa fa-instagram fa-2x icon"></i>
           </a>
           <a
-            href="https://s3.amazonaws.com/handshake.production/documents/documents/019/584/025/original/Derrick_Zhen_Resume_Sept_2020.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAILGI2B7PIOQKG5EA%2F20200913%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200913T062828Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host&X-Amz-Signature=54bafb2af7f5a725c8c4359410e61d9d84579a980527865ecf76389ccffa0e6b"
+            href="https://app.joinhandshake.com/documents/19584025/download"
             target="_blank"
             rel="noopener noreferrer"
           >
